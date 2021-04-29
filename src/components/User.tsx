25676a56a469b6bca07e67aa39d0b805b445e55e
Trevor Marshall <trevorhmarshall99@gmail.com>
2021-04-28 22:11:10 -0400

    Temporarily fixed setScore on user component

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,28 +1,33 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import HighScore from './HighScore';
 import { db } from '../firebase/firebase';
 
-function User(props: any) {
+function User({user, highScr, setScore}: any) {
   
-  // const wpmRef = db.collection("users").doc(props.user?.uid);
-  //   wpmRef.get().then((doc) => {
-  //     props.setScore(doc.data());
-  //   });
+  const seeHighScore = () => {
+    const wpmRef = db.collection("users").doc(user?.uid);
+    wpmRef.get().then((doc) => {
+      setScore(doc.data());
+    });
+  }
+  
+  
+  const isLoggedIn = user;
 
-  const isLoggedIn = props.user;
-  console.log(props);
+
   if(isLoggedIn){
   return (
     <div>
       <div>
-      {props.user.name}
-      <img src={props.user.photo} alt={`${props.user.name}'s Profile Pic`}/>
+      {user.name}
+      <img src={user.photo} alt={`${user.name}'s Profile Pic`}/>
       || Profile pic goes here
       </div>
       <div>
       This is where the user's stats will be
       </div>
-      <HighScore highScr={props.highScr}/>
+      <button onClick={seeHighScore}>Click here to update your highscore</button>
+      <HighScore highScr={highScr}/>
     </div>
   );} else {
     return (<h2>Once you are signed in your stats will be shown here.</h2>)
