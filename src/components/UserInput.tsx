b53a7b6816eb5b507d9ca94e426cd60bf58e8a5d
Trevor Marshall <trevorhmarshall99@gmail.com>
2021-04-28 20:43:52 -0400

    clean up

--- src/components/UserInput.tsx
+++ src/components/UserInput.tsx
@@ -1,39 +1,11 @@
-import React, { FunctionComponent, useRef, useState, useEffect } from 'react';
+import React, { FunctionComponent, useRef } from 'react';
 import { useTyping } from '../state/context';
-
-// Key down event
-function useKey(key: any) {
-  const [pressed, setPressed] = useState(false);
-
-  const match = (event: any) => key.toLowerCase() === event.key.toLowerCase();
-
-  const onDown = (event: any) => {
-    if(match(event)) setPressed(true);
-  }
-
-  const onUp = (event: any) => {
-    if(match(event)) setPressed(false);
-  }
-
-  useEffect(() => {
-    window.addEventListener('keydown', onDown);
-    window.addEventListener('keyup', onUp);
-    return () => {
-      window.removeEventListener('keydown', onDown);
-      window.removeEventListener('keyup', onUp);
-    }
-  }, [key])
-  
-  return pressed;
-}
 
 export const UserInput: FunctionComponent = () => {
   const {
     state: { input, disabledTxtArea }, 
     onInput, onReset, updateQuote, updateTxtAreaDisabled
   } = useTyping();
-
-  const tab = useKey('tab');
 
   
   const inputRef = useRef<HTMLTextAreaElement>(null);
